7d906fc3f627ac1374ebb296494b7e342e9264e7
a-jafarzadeh <a.jafarzadeh1998@gmail.com>
2019-12-27 01:23:15 +0330

    company profile complete

--- Company/forms.py
+++ Company/forms.py
@@ -40,3 +40,58 @@ class SignUpForm(forms.ModelForm):
 
         return data
     
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = models.CompanyProfile
+        fields = [ 'photo' ,'name' , 'dateOfFoundation' ,'job_field' ,'website',
+                   'phoneNumber' ,'address' ]
+        widgets={
+            'photo'            : forms.FileInput(attrs={'name':'photo'}),
+            'name'             : forms.TextInput(attrs={'class':"form-control input-lg" ,'placeholder':"Company Name" ,'name':"name"}),
+            'phoneNumber'      : forms.TextInput(attrs={'class':"form-control input-lg" ,'placeholder':"Phone Number" ,'name':"phoneNumber"}),
+            'dateOfFoundation' : forms.NumberInput(attrs={'class':"form-control input-lg" ,'placeholder':"dateOfFoundation" ,'name':"dateOfFoundation"}),
+            'address'          : forms.Textarea(attrs={"name":"address" ,'class':"form-control input-lg" ,'placeholder':"Address" ,'cols':"30" ,'rows':"5"}),
+            'job_field'        : forms.TextInput(attrs={'class':"form-control input-lg" ,'placeholder':"Job Field" ,'name':"job_field"}),
+            'website'          : forms.TextInput(attrs={'class':"form-control input-lg" ,'placeholder':"Web Site" ,'name':"website"}),
+        }
+
+    def clean_photo(self):
+        data = self.cleaned_data.get('photo')
+
+        return data
+    
+    def clean_name(self):
+        data = self.cleaned_data["name"]
+        
+        return data
+    
+    def clean_address(self):
+        data = self.cleaned_data["address"]
+        
+        return data
+    
+    def clean_dateOfFoundation(self):
+        data = self.cleaned_data["dateOfFoundation"]
+        
+        return data
+    
+    def clean_job_field(self):
+        data = self.cleaned_data["job_field"]
+        
+        return data
+    
+    def clean_website(self):
+        data = self.cleaned_data["website"]
+        
+        return data
+    
+    def clean_phoneNumber(self):
+        data = self.cleaned_data["phoneNumber"]
+        try:
+            int(data)
+        except ValueError:
+            raise forms.ValidationError('It should be numeric.')
+
+        if len(data) != 11:
+            raise forms.ValidationError('It should be 11 digit.')
+        return data
--- Company/views.py
+++ Company/views.py
@@ -3,7 +3,7 @@ from django.views import generic
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from . import models
+from . import models ,forms
 
 class Index(LoginRequiredMixin ,generic.TemplateView):
     login_url = reverse_lazy('login')
@@ -28,7 +28,33 @@ class CompanyList(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        return context
+        return context    
 
-class Profile(generic.TemplateView):
+class Profile(LoginRequiredMixin ,generic.TemplateView):
     template_name = 'company/editProfile.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = forms.ProfileForm(instance=self.request.user.company.companyprofile)
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        company = models.Company.objects.filter(user=request.user)
+        if company.count() == 0:
+            return HttpResponseRedirect(reverse('index'))
+        return super().get(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        form = forms.ProfileForm(request.POST ,request.FILES)
+        company = models.Company.objects.filter(user=request.user).first()
+        profile = company.companyprofile
+        if form.is_valid():
+            profile.photo            = form.cleaned_data['photo']
+            profile.name             = form.cleaned_data['name']
+            profile.address          = form.cleaned_data['address']
+            profile.job_field        = form.cleaned_data['job_field']
+            profile.website          = form.cleaned_data['website']
+            profile.phoneNumber      = form.cleaned_data['phoneNumber']
+            profile.dateOfFoundation = form.cleaned_data['dateOfFoundation']
+            profile.save()
+        return HttpResponseRedirect(reverse('company:profile'))
